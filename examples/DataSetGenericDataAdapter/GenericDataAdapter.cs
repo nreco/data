9c25e0bf3d9b011d984f42461f19225aef334d13
Vitalii Fedorchenko <fedorchenko@gmail.com>
2017-10-04 22:39:33 +0300

    Example for DataSet CRUD with GenericDataAdapter #42

---
+++ examples/DataSetGenericDataAdapter/GenericDataAdapter.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Common;
+using System.Data;
+
+using NReco.Data;
+
+namespace DataSetGenericDataAdapter
+{
+
+	/// <summary>
+	/// Generic implementation of <see cref="IDbDataAdapter"/>.
+	/// </summary>
+	/// <remarks>Note: this implementation ignores tables and columns mapping. You may enhance the code if you need this feature.</remarks>
+    public class GenericDataAdapter : System.Data.Common.DbDataAdapter {
+
+		IDbCommandBuilder CmdBuilder;
+		DbConnection Conn;
+
+		public GenericDataAdapter(IDbCommandBuilder cmdBuilder, DbCommand selectCmd) {
+			CmdBuilder = cmdBuilder;
+			SelectCommand = selectCmd;
+		}
+
+		public GenericDataAdapter(IDbCommandBuilder cmdBuilder, DbConnection conn) {
+			CmdBuilder = cmdBuilder;
+			Conn = conn;
+		}
+
+		IEnumerable<KeyValuePair<string,IQueryValue>> GetChangeset(DataTable t) {
+			var res = new List<KeyValuePair<string, IQueryValue>>(t.Columns.Count);
+			foreach (DataColumn col in t.Columns)
+				if (!col.AutoIncrement && !col.ReadOnly) {
+					res.Add(new KeyValuePair<string, IQueryValue>(col.ColumnName, new QVar(col.ColumnName).Set(null) ));
+				}
+			return res.ToArray();
+		}
+
+		const string OriginalSuffix = "__ORIGINAL";
+
+		void InitDbCmd(DbCommand cmd, DataTable t) {
+			foreach (DbParameter p in cmd.Parameters) {
+				if (p.SourceColumn != null) {
+					if (p.SourceColumn.EndsWith(OriginalSuffix)) {
+						p.SourceColumn = p.SourceColumn.Substring(0, p.SourceColumn.Length - OriginalSuffix.Length);
+						p.SourceVersion = DataRowVersion.Original;
+					} else {
+						p.SourceVersion = DataRowVersion.Current;
+					}
+					var col = t.Columns[p.SourceColumn];
+					// you may use column metadata to initialize DbParameter in a special way if needed
+				}
+			}
+			if (SelectCommand != null && SelectCommand.Connection != null) { 
+				cmd.Connection = SelectCommand.Connection;
+			} else {
+				cmd.Connection = Conn;
+			}
+		}
+
+		QNode ComposePkCondition(DataTable t) {
+			var pkCondition = new QGroupNode(QGroupType.And);
+			foreach (DataColumn col in t.PrimaryKey) {
+				pkCondition.Nodes.Add(
+					(QField)col.ColumnName == new QVar(col.ColumnName+OriginalSuffix).Set(null) );
+			}
+			return pkCondition;
+		}
+
+		protected override int Update(DataRow[] dataRows, DataTableMapping tableMapping) {
+			// generate commands by first row table schema
+			if (dataRows.Length>0) {
+				var tbl = dataRows[0].Table;
+				var changeset = GetChangeset(tbl);
+				InsertCommand = (DbCommand)CmdBuilder.GetInsertCommand(tbl.TableName, changeset);
+				InitDbCmd(InsertCommand, tbl);
+
+				if (tbl.PrimaryKey!=null && tbl.PrimaryKey.Length>0) {
+					var pkQuery = new Query(tbl.TableName, ComposePkCondition(tbl) );
+					UpdateCommand = (DbCommand)CmdBuilder.GetUpdateCommand(pkQuery, changeset);
+					InitDbCmd(UpdateCommand, tbl);
+
+					DeleteCommand = (DbCommand)CmdBuilder.GetDeleteCommand(pkQuery);
+					InitDbCmd(DeleteCommand, tbl);
+				}
+			}
+			
+
+			return base.Update(dataRows, tableMapping);
+		}
+
+
+	}
+
+}
---
+++ examples/DataSetGenericDataAdapter/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Data.Common;
+
+using NReco.Data;
+
+namespace DataSetGenericDataAdapter
+{
+	class Program {
+		static void Main(string[] args) {
+
+			GenericDataAdapterForSqlite();
+
+			GenericDataAdapterForMySql();
+		}
+
+		static void GenericDataAdapterForSqlite() {
+			Console.WriteLine("Sqlite database (northwind.db)");
+
+			var dbFactory = new DbFactory(Microsoft.Data.Sqlite.SqliteFactory.Instance) {
+				LastInsertIdSelectText = "SELECT last_insert_rowid()",
+				IdentifierFormat = "\"{0}\""
+			};
+			var dbCmdBuilder = new NReco.Data.DbCommandBuilder(dbFactory);
+
+			var sqliteDbPath = Path.Combine(Directory.GetCurrentDirectory(), "northwind.db");
+			var sqliteConnStr = String.Format("Data Source={0}", sqliteDbPath);
+
+			using (var conn = dbFactory.CreateConnection()) {
+				conn.ConnectionString = sqliteConnStr;
+
+				var selectCmd = dbCmdBuilder.GetSelectCommand(new Query("Employees"));
+				selectCmd.Connection = conn;
+
+				var dsDataAdapter = new GenericDataAdapter(dbCmdBuilder, (DbCommand)selectCmd);
+				var ds = new DataSet();
+				dsDataAdapter.Fill(ds, "Employees");
+
+				Console.WriteLine("Fill: loaded {0} rows", ds.Tables["Employees"].Rows.Count);
+
+				// lets set PK
+				ds.Tables["Employees"].PrimaryKey = new[] { ds.Tables["Employees"].Columns["EmployeeID"] };
+				ds.Tables["Employees"].Columns["Deleted"].ReadOnly = true;  // do not insert/update this column
+
+				// and modify some rows
+				ds.Tables["Employees"].Rows.Find(1)["FirstName"] = "Nancy1";
+				var newRow = ds.Tables["Employees"].NewRow();
+				newRow["EmployeeID"] = 10;
+				newRow["FirstName"] = "John";
+				newRow["LastName"] = "Smith";
+				newRow["ReportsTo"] = 2;
+				newRow["Deleted"] = 1;  // this will be ignored 
+				ds.Tables["Employees"].Rows.Add(newRow);
+
+				Console.WriteLine("Update (insert+update): affected {0} rows", dsDataAdapter.Update(ds.Tables["Employees"]));
+
+				ds.Tables["Employees"].Rows.Find(10).Delete();
+				Console.WriteLine("Update (delete): affected {0} rows", dsDataAdapter.Update(ds.Tables["Employees"]));
+
+			}
+			Console.WriteLine();
+		}
+
+		static void GenericDataAdapterForMySql() {
+			Console.WriteLine("Mysql database (sample server, may respond slowly)");
+
+			var dbFactory = new DbFactory(MySql.Data.MySqlClient.MySqlClientFactory.Instance) {
+				LastInsertIdSelectText = "SELECT LAST_INSERT_ID()",
+				IdentifierFormat = "`{0}`"
+			};
+			var dbCmdBuilder = new NReco.Data.DbCommandBuilder(dbFactory);
+
+			var mysqlConnStr = "Server=db4free.net;Database=nreco_sampledb;Uid=nreco;Pwd=HRt5UbVD;";
+			using (var conn = dbFactory.CreateConnection()) {
+				conn.ConnectionString = mysqlConnStr;
+
+				var selectCmd = dbCmdBuilder.GetSelectCommand(new Query("orders"));
+				selectCmd.Connection = conn;
+
+				var dsDataAdapter = new GenericDataAdapter(dbCmdBuilder, (DbCommand)selectCmd);
+				var ds = new DataSet();
+				dsDataAdapter.Fill(ds, "orders");
+
+				Console.WriteLine("Fill: loaded {0} rows", ds.Tables["orders"].Rows.Count);
+			}
+		}
+	}
+}
